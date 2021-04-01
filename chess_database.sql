b7dea38edf1ec8a9d906b8a03301c88e26b2a4e1
Sasoun Krikorian <sasounkr@usc.edu>
2021-03-31 19:09:49 -0700

    Update chess_database.sql

--- chess_database.sql
+++ chess_database.sql
@@ -1,19 +1,7 @@
--- comment this out if you like
+-- comment out drop when necessary
 DROP DATABASE IF EXISTS chess;
 CREATE DATABASE chess;
 USE chess;
-
-CREATE TABLE Game (
-	game_id INT PRIMARY KEY,
-    FOREIGN KEY fk1(white_player_id) REFERENCES User(user_id),
-    FOREIGN KEY fk2(black_player_id) REFERENCES User(user_id),
-    -- can pass only those 3 strings, any other string will give an error
-    game_status ENUM('win', 'loss', 'tie'),
-    start_time DATETIME,
-    end_time DATETIME,
-    white_player_elo INT,
-    black_player_elo INT
-);
 
 CREATE TABLE User (
 	user_id INT PRIMARY KEY,
@@ -27,4 +15,18 @@ CREATE TABLE User (
     num_losses INT,
     num_ties INT,
     num_games INT
+);
+
+CREATE TABLE Game (
+	game_id INT PRIMARY KEY,
+    white_player_id INT,
+    black_player_id INT,
+    FOREIGN KEY (white_player_id) REFERENCES User(user_id),
+    FOREIGN KEY (black_player_id) REFERENCES User(user_id),
+    -- can pass only those 3 strings, any other string will give an error
+    game_status ENUM('win', 'loss', 'tie'),
+    start_time DATETIME,
+    end_time DATETIME,
+    white_player_elo INT,
+    black_player_elo INT
 );
