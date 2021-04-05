b19dc9b4531bc9578f2e7d75e3e9b99cbe850ae9
Sasoun Krikorian <sasounkr@usc.edu>
2021-04-04 20:58:58 -0700

    added a little to Query class
    YAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYYYAYAYAYAYAYAYY

--- SasounStuff/SQLStuff/src/sqlstuff/Query.java
+++ SasounStuff/SQLStuff/src/sqlstuff/Query.java
@@ -1,6 +1,23 @@
 package sqlstuff;
 
+import java.lang.IllegalArgumentException;
+
 public class Query {
+	private int numAttempts = 0;
+	
+	public int getNumAttempts() {
+		return numAttempts;
+	}
+	
+	public void setNumAttempts(int numAttempts) {
+		if (numAttempts < 0)
+			throw new IllegalArgumentException("Tried to set numAttempts to " + numAttempts);
+		this.numAttempts = numAttempts;
+	}
+	
+	static void sendEmail(String username) {
+		// TODO: how the hell do i send an email?
+	}
 	
 	
 	public static void main(String args[]) {
