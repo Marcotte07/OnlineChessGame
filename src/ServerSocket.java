4305e5089a1dcc74704e47f1bad084b2830ea545
Sasoun Krikorian <sasounkr@usc.edu>
2021-04-18 19:17:12 -0700

    Create ServerSocket.java

---
+++ src/ServerSocket.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.util.Vector;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/ws")
+public class ServerSocket {
+
+	private static Vector<Session> sessionVector = new Vector<Session>();
+	
+	@OnOpen
+	public void open(Session session) {
+		System.out.println("Connection made!");
+		sessionVector.add(session);
+	}
+	
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		System.out.println(message);
+		for(Session s : sessionVector) {
+			if (s != this)
+				s.getAsyncRemote().sendText(message);
+		}
+	}
+	
+	@OnClose
+	public void close(Session session) {
+		System.out.println("Disconnecting!");
+		sessionVector.remove(session);
+	}
+	
+	@OnError
+	public void error(Throwable error) {
+		System.out.println("Error!");
+	}
+}
